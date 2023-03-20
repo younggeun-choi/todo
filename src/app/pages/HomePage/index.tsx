750593df01a02fac18c0efadc93a789c310a2430
YoungGeun CHOI <im@ygchoi.com>
2023-03-20 17:01:24 +0900

    feature: enrich TodoItem with props

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import styled from 'styled-components';
 import TodoInput from 'app/components/TodoInput';
+import TodoItem from 'app/components/TodoItem';
 
 const Wrapper = styled.div`
   width: 100%;
@@ -25,18 +26,10 @@ const Title = styled.h1`
 `;
 
 const TodoList = styled.div``;
-const TodoItem = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 15px 25px;
-  width: 100%
-  font-size: 1.2em;
-  border-bottom: 1px solid #eee;
-`;
 
-const TodoCheck = styled.input`
-  margin-right: 15px;
-`;
+// const TodoCheck = styled.input`
+//   margin-right: 15px;
+// `;
 
 export function HomePage() {
   return (
@@ -50,10 +43,22 @@ export function HomePage() {
           <Title>할 일</Title>
           <TodoInput />
           <TodoList>
-            <TodoItem>
-              <TodoCheck type="checkbox" />
-              나는 투두야
-            </TodoItem>
+            <TodoItem
+              todo={{
+                id: '1',
+                completed: false,
+                content: '투두 입니다 1.',
+                editing: false,
+              }}
+            ></TodoItem>
+            <TodoItem
+              todo={{
+                id: '2',
+                completed: true,
+                content: '투두 입니다 2.',
+                editing: false,
+              }}
+            ></TodoItem>
           </TodoList>
         </Box>
       </Wrapper>
